eddb0e1fc3f1d67e4ef296904065b86feea9feb4
Aayush Bhandari <aayushbhandari6789@gmail.com>
2023-12-18 20:09:30 +0545

    Initial commit

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Layout = () => {
+	return (
+        <>
+            <div className='sidebar-section'>
+            </div>
+        </>
+    )
+};
+
+export default Layout;
