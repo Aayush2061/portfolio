2c925d298944c83bfa93ccea1f0d76863ef6d0f3
Aayush Bhandari <aayushbhandari6789@gmail.com>
2023-12-19 21:06:01 +0545

    Add home section to the app

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import './home.css'
+import Typewriter from 'typewriter-effect'
+import Resume from '../../assets/resume.txt' // Replace this with your resume file
+
 const Home = () => {
   return (
-    <div>
-        <h1>Home Page</h1>
-    </div>
+    <>
+      <div className="container-fluid home-container">
+        <div className="container home-content">
+          <h1>Hi I'm a</h1>
+          <h2>
+            <Typewriter
+              options={{
+                strings: ['Web Developer !', 'Full Stack Developer !', 'Mern Stack Developer !'],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h2>
+          <div className='home-buttons'>
+            <button className='btn btn-hire'>Hire Me</button>
+            <a className='btn btn-cv' href={Resume} download="Aayush.pdf">My Resume</a>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
