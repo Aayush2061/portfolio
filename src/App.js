6941f3394b6b017658d027acfa0c7bc6a5f9c1f2
Aayush Bhandari <aayushbhandari6789@gmail.com>
2023-12-21 00:09:17 +0545

    Add a about page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Layout from './components/Layout/Layout';
+import About from './pages/About/About';
 
 function App() {
 	return (
 		<>
 			<Layout />
+			<div className="container about-container">
+				<About />
+			</div>
+
 		</>
 	);
 }
---
+++ src/pages/About/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./About.css"
+const About = () => {
+    return (
+        <div className="about">
+            <div className="row">
+                <div className="col-md-6 col-xl-6 col-lg-6 col-xs-12 about-img">
+                    <img src="profilepic.jpg" alt='Profile_Pic' />
+                </div>
+                <div className="col-md-6 col-xl-6 col-lg-6 col-xs-12 about-content">
+                    <h1>About Me</h1>
+                    <p>Lorem ipsum dolor sit ametatum dolor ut, quia magni eum nostrum ducimus quae minus, id debitis pariatur dolore in reprehenderit dolorum voluptatibus porro fuga. Eos enim maiores in molestias soluta, assumenda voluptatem neque? Quibusdam, ducimus nihil reprehenderit esse nulla cumque possimus cum totam, accusamus harum, nostrum aut provident distinctio inventore consequuntur laudantium delectus! Deleniti incidunt quibusdam rem alias dignissimos deserunt impedit sunt repellendus mollitia. Similique quasi aut atque commodi recusandae minima distinctio tempore laborum cumque sint necessitatibus et provident veniam, molestiae neque ipsam. Culpa ipsam quia possimus aspernatur sapiente, impedit, asperiores iste accusantium quam quaerat vitae voluptatem distinctio maxime nostrum dignissimos saepe temporibus ut nemo. Molestias enim praesentium laudantium vel nostrum distinctio exercitationem! Explicabo velit culpa odit quidem. Id explicabo nobis officia amet aperiam expedita doloremque corrupti mollitia, perferendis animi, beatae, at aspernatur ut officiis? Animi cupiditate libero eaque! Sed vitae asperiores minus, incidunt porro nostrum aliquid fugiat id!</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
