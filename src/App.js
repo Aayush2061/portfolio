b6eae6bc9338f9e2c4dea39e52b0495278ab74b2
Aayush Bhandari <aayushbhandari6789@gmail.com>
2023-12-22 17:39:17 +0545

    Add a tech stack style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Layout from './components/Layout/Layout';
 import About from './pages/About/About';
+import Techstack from './pages/Techstack/Techstack';
 
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 			<Layout />
 			<div className="container about-container">
 				<About />
+				<Techstack />
 			</div>
 
 		</>
---
+++ src/utils/TechstackList.js
@@ -0,0 +1,70 @@
+import { FaHtml5, FaCss3, FaBootstrap } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { RiReactjsFill } from "react-icons/ri";
+import { SiMui } from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa";
+import { SiExpress, SiMongodb } from "react-icons/si";
+import { FaGithub } from "react-icons/fa";
+import { DiJqueryLogo } from "react-icons/di";
+export const TechStackList = [
+    {
+        _id: 1,
+        name: 'HTML',
+        icon: FaHtml5,
+    },
+    {
+        _id: 2,
+        name: 'CSS',
+        icon: FaCss3,
+    }
+    ,
+    {
+        _id: 3,
+        name: 'Bootstrap',
+        icon: FaBootstrap,
+    }
+    ,
+    {
+        _id: 5,
+        name: 'Javascript',
+        icon: IoLogoJavascript,
+    },
+    {
+        _id: 6,
+        name: 'jQuery',
+        icon: IoLogoJavascript,
+    },
+    {
+        _id: 7,
+        name: 'React JS',
+        icon: RiReactjsFill,
+    }
+    ,
+    {
+        _id: 8,
+        name: 'React MUI',
+        icon: SiMui,
+    },
+    {
+        _id: 9,
+        name: 'Node JS',
+        icon: FaNodeJs,
+    },
+    {
+        _id: 10,
+        name: 'Express JS',
+        icon: SiExpress,
+    },
+    {
+        _id: 11,
+        name: 'Mongodb',
+        icon: SiMongodb,
+    }
+    ,
+    {
+        _id: 12,
+        name: 'Git/Github',
+        icon: FaGithub,
+    }
+
+]
